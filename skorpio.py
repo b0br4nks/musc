16ddd3331c52fa79e75aa45b2a5a8a9de717ffa1
noamzeitoun <zeitoun.noam@gmail.com>
2022-12-24 12:33:59 +0100

    Add `rot_13` example

--- skorpio.py
+++ skorpio.py
@@ -652,6 +652,8 @@ def type_check_program(program: Program):
 
                 if a_type == b_type and a_type == DataType.INT:
                     stack.append((DataType.INT, op.token))
+                elif a_type == b_type and a_type == DataType.BOOL:
+                    stack.append((DataType.BOOL, op.token))
                 else:
                     compiler_error_with_expansion_stack(op.token, "invalid argument type for BAND intrinsic")
                     exit(1)
@@ -1221,7 +1223,7 @@ INTRINSIC_BY_NAMES = {
     ">>": Intrinsic.RSH,
     "<<": Intrinsic.LSH,
     "or": Intrinsic.BOR,
-    "&": Intrinsic.BAND,
+    "and": Intrinsic.BAND,
     "cp": Intrinsic.DUPL,
     "~": Intrinsic.SWAP,
     "!": Intrinsic.DROP,
